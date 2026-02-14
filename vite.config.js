8ec264d0be7838fc735172dfe64a256c56f095b1
nargiiziii <ibrahimlinargiz169@gmail.com>
2026-02-14 05:18:39 -0800

    some deployment issues have been resolved

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/chery-e5-landing/",
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
-  }
-})
+    open: true,
+  },
+});
